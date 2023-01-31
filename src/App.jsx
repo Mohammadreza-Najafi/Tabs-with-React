547c48b3da52e225e91cfc5956363da33e2cec46
Mohammadreza Najafi <mohammadreza.najafi.2002@gmail.com>
2023-01-31 21:31:18 +0330

    Complete logic program

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
-import { useState } from 'react'
+import {useEffect, useState} from 'react'
+// import { FaAngleDoubleRight } from 'react-icons/fa'
 
 const url ='https://course-api.com/react-tabs-project'
+
 function App() {
     const [loading,setLoading]=useState(true)
     const [jobs,setJobs] = useState([])
@@ -12,26 +14,60 @@ function App() {
         setJobs(newJobs)
         setLoading(false)
     }
+
+    useEffect(() =>{
+        fetchJobs()
+    },[])
+
+    if(loading){
+        return (
+            <section className="section loading">
+                <h1>Loading...</h1>
+            </section>
+        )
+    }
+
+    const {company,dates,duties,title} = jobs[value]
+
   return (
    <section className="section">
+
        <div className="title">
            <h2>experience</h2>
            <div className="underline"></div>
        </div>
+
        <div className="jobs-center">
+
            <div className="btn-container">
-               <button></button>
+               {jobs.map( (item,index)=>{
+                           return(
+                               <button
+                                   key={item.id}
+                                   onClick={() => setValue(index)}
+                                   className={`job-btn ${index === value && 'active-btn'}`}>
+                                   {item.company}
+                               </button>
+                           )
+               })}
            </div>
 
            <article className="job-info">
-               <h3></h3>
-               <h4></h4>
-               <p></p>
-               <div>
-                   FaA
-               </div>
+               <h3>{title}</h3>
+               <h4>{company}</h4>
+               <p className="job-date">{dates}</p>
+               {duties.map((duty,index) => {
+                   return(
+                       <div key={index} className="job-desc">
+                           <p>{duty}</p>
+                       </div>
+                   )
+               })}
+
            </article>
+
        </div>
+
        <button type="button" className="btn">
            more info
        </button>
