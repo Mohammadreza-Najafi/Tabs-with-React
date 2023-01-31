d053c9cfa58f53d667eeb77074ddf00715803f39
Mohammadreza Najafi <mohammadreza.najafi.2002@gmail.com>
2023-01-31 14:41:35 +0330

    feat:Complete HTML part of program

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,33 @@
 import { useState } from 'react'
 
+const url ='https://course-api.com/react-tabs-project'
 function App() {
+
   return (
-      <div>
-          erg
-      </div>
+   <section className="section">
+       <div className="title">
+           <h2>experience</h2>
+           <div className="underline"></div>
+       </div>
+       <div className="jobs-center">
+           <div className="btn-container">
+               <button></button>
+           </div>
+
+           <article className="job-info">
+               <h3></h3>
+               <h4></h4>
+               <p></p>
+               <div>
+                   FaA
+               </div>
+           </article>
+       </div>
+       <button type="button" className="btn">
+           more info
+       </button>
+
+   </section>
   )
 }
 
