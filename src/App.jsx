40fed71e5cf2fd21d0e5588e6f7d707461881c19
Mohammadreza Najafi <mohammadreza.najafi.2002@gmail.com>
2023-01-31 14:44:31 +0330

    feat:set some state and implement logic fetch data from server

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,16 @@ import { useState } from 'react'
 
 const url ='https://course-api.com/react-tabs-project'
 function App() {
+    const [loading,setLoading]=useState(true)
+    const [jobs,setJobs] = useState([])
+    const [value,setValue]=useState(0)
 
+    const fetchJobs = async () =>{
+        const response = await fetch(url)
+        const newJobs = await response.json()
+        setJobs(newJobs)
+        setLoading(false)
+    }
   return (
    <section className="section">
        <div className="title">
